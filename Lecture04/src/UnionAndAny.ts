9c92c41b0b20cbd05b6102083db00a1c5df87320
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-22 00:18:54 +0530

    code push

---
+++ Lecture04/src/UnionAndAny.ts
@@ -0,0 +1,19 @@
+// Yha p hamm generally Union And Any ke bare m learn karne ja rhe! okkh!..
+// union means like kisi bhi element ka ham kitne bhi dataType de sakte okkh!.. for and example!..
+
+let name: string | number = "Parvej";
+
+// ye hai wo name union hai okkh!.
+
+let seat: "ariell" | "middle" | "window" = "window";
+
+// Yha p seat jo hai wo above wale dataType m s hi kuch hoga okh!.. and kuch extra nahi ho sakta! yahi hoga bass!..
+
+// but kahi kahi p hamne any bhi dekhha hai uska generally mtlb ye hota hai like ki koi type s lena dena hi nahi hai
+// kuch bhi ho sakta hai okkh!.. koi bhi type aa sakta haii hame actaully type s lena dena hi nahhi hai okk!..
+
+let unknownPerson: any = "Hi there!.."; // to iska mtlb yahi hai ki unknownPerson kuch bhi ho sakta hai kisii bhi type ka and hame type s lena dena hi nahi hai !..
+
+// hamne do chize learn hi hai yha p like uniion hai and any okh!,
+// union means kisi bhi element ka zada type dena okkh! limme name: string | number | etc  ye tera union hai okkh!..
+// iske baad tera any ata hai okkh! isme generally dataType s kuch lena dena nahi hota kuch bhi ho sakta hai !...
