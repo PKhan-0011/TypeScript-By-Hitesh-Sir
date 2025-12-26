6ffd9ddcf990fb61a66ffa4d9f8db99432039d55
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-12-26 13:08:03 +0530

    code push

--- Lecture13/src/axios.ts
+++ Lecture13/src/axios.ts
@@ -2,6 +2,8 @@
 // to uske sath hi saraData type aa jata hai agarr nahi bhi ata to hamm npm i -D @type/ karke hamm wo type declare kar lete hai okh!..
 // but in case fir bhi nahi ata hai to hmm .d.ts m le lete hai okh!..
 
+// axios always return axiosResponse and uske andar hamm generic lete hai okkh!..
+
 import axios from "axios";
 import type { AxiosResponse } from "axios";
 
@@ -23,6 +25,34 @@ export const fetchData = async () => {
     // kya smjh aya yhi ki like respoonse jo hai wo generic returnn karta hai okkh!..
     console.log(response);
     console.log(response.data);
+  } catch (e) {
+    const error = e as Error;
+    console.log(error);
+  }
+};
+
+interface Data {
+  id: string;
+  userId: number;
+  title: string;
+}
+
+const fetchData02 = async () => {
+  try {
+    // axios always return a generic okkh!..
+    // const response: AxiosResponse<Data> = await axios.get(
+    //   "http:localhost:300/getData"
+    // );
+    // console.log(response.data);
+    // console.log(response.headers);
+
+    const resposne = await fetch("");
+
+    if (!resposne.ok) {
+      console.log("data fetch hi nahi hua hai okkh!..");
+    }
+
+    const data: Data = await resposne.json();
   } catch (e) {
     const error = e as Error;
     console.log(error);
